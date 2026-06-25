ce1da7511a774c6c3844dbc0a3c7e3957a4ab21f
Olviia Stroivans <olviia.stroivans@gmail.com>
2026-06-25 14:17:53 +0100

    tags added check

--- Assets/Scripts/Core/Player/TagSet.cs
+++ Assets/Scripts/Core/Player/TagSet.cs
@@ -1,12 +1,72 @@
-namespace Core.Player
-{
-    /// <summary>
-    /// this class is for the tags - the state the player is currently in
-    /// for example, stunned, frozen, in fighting mode etc. thus the tags
-    /// can tell the modules if the modules can execute themselves
-    /// </summary>
-    public class TagSet
-    {
-        
-    }
-}
+using System;
+using System.Collections.Generic;
+
+ namespace Core.Player
+ {
+     /// <summary>
+     /// List of tags. tags are the states of the actor: fighting, frozen, etc.
+     /// </summary>
+     [System.Flags]
+     public enum Tag
+     {
+         None        = 0,
+         //example:
+         // Stunned     = 1 << 0,
+         // Frozen      = 1 << 1,
+         //
+     }
+     
+     /// <summary>
+     /// this class is for the tags - the state the player is currently in
+     /// for example, stunned, frozen, in fighting mode etc. thus the tags
+     /// can tell the modules if the modules can execute themselves
+     /// </summary>
+     public class TagSet
+     {
+         private readonly Dictionary<Tag, int> currentTags = new();
+         private Tag present;
+
+         public event Action<Tag> Added;
+         public event Action<Tag> Removed;
+
+         //something with bits aggregation
+         public bool HasAny(Tag mask) => (present & mask) != 0;
+         public bool HasAll(Tag mask) => (present & mask) == mask;
+
+         public void Add(Tag t)
+         {
+             foreach (var bit in Bits(t))
+             {
+                 currentTags.TryGetValue(bit, out int c);
+                 currentTags[bit] = c + 1;
+                 if (c == 0)
+                 {
+                     present |= bit;
+                     Added?.Invoke(bit);
+                 }
+             }
+         }
+
+         public void Remove(Tag t)
+         {
+             foreach (var bit in Bits(t))
+             {
+                 if (!currentTags.TryGetValue(bit, out int c) || c == 0) continue;
+                 currentTags[bit] = --c;
+                 if (c == 0)
+                 {
+                     present &= ~bit;
+                     Removed?.Invoke(bit);
+                 }
+             }
+         }
+
+         private static IEnumerable<Tag> Bits(Tag t)
+         {
+             for (Tag b = (Tag)1; (int)b != 0 && b <= t; b = (Tag)((int)b << 1))
+             {
+                 if ((t & b) != 0) yield return b;
+             }
+         }
+     }
+ }
